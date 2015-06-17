fd60f8bd035e0693347388033d73d9b0645c40f1
Arsalan Vakili <arsenal.v.001@gmail.com>
2015-06-17 17:12:51 -0400

    Initial Commit

---
+++ Auto-Layout StoryBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Auto-Layout StoryBoard
+//
+//  Created by Arsalan Vakili on 2015-06-17.
+//  Copyright (c) 2015 Arsalan Vakili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Auto-Layout StoryBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Auto-Layout StoryBoard
+//
+//  Created by Arsalan Vakili on 2015-06-17.
+//  Copyright (c) 2015 Arsalan Vakili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
